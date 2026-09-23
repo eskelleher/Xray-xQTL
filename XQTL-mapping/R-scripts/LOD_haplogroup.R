e450af906e6b54215a588405f5c54c695a3c2a87
eskelleher <43545136+eskelleher@users.noreply.github.com>
2026-09-22 19:00:48 -0500

    Add files via upload

---
+++ XQTL-mapping/R-scripts/LOD_haplogroup.R
@@ -0,0 +1,319 @@
+library(tidyverse)
+
+#import haplotypes for 200 Kb, 500 Kb and 1Mb windows
+hap200 <- read_delim("allhaps.200Kb.txt",col_names=TRUE) 
+hap500 <- read_delim("allhaps.500Kb.txt",col_names=TRUE) 
+hap1000 <- read_delim("allhaps.1Mb.txt",col_names=TRUE) 
+
+#make AB8 values into B8 so they are sorted last.
+hap200$founder[hap200$founder == "AB8"] <- "B8"
+hap500$founder[hap500$founder == "AB8"] <- "B8"
+hap1000$founder[hap1000$founder == "AB8"] <- "B8"
+
+#make chr into a factor
+hap200$chr <- as.factor(hap200$chr)
+hap500$chr <- as.factor(hap500$chr)
+hap1000$chr <- as.factor(hap1000$chr)
+###extract windows from both sides of the 3rd chromosome centromere, exclude windows were AF is not estimated
+###we only need values from one replicate:treatment. We will use identical AF estimates to detect pairs and groups of haplotypes that cannot be differentiated from each other within a window.
+
+
+
+#the function below will take a list of 8 haplotype frequencies in the same position, sort them, keep the unique values first founder name (in ascending order 1-8)
+#the each uniquely identifiable haplotypes is then assigned a "haplogroup" name based o the first founder that has this haplotype
+haplogroup_name <- function(df){
+  df <- data.frame(df)
+  left_join(df, arrange(df, freq,founder) %>% distinct(freq,.keep_all=TRUE) %>% rename(c("haplogroup"="founder"))) 
+}
+
+cent.200 <- rbind(filter(hap200, chr == "chr3R", pos < 7307440, !is.na(freq), pool== "A.C") %>% select(-pool),
+                 filter(hap200, chr == "chr3L", pos > 22586280, !is.na(freq), pool== "A.C") %>% select(-pool))
+## groups data in cent.3R by chromosome and position and assigns a founder name to each unique haplogroup
+cent.200.haplogroups <- cent.200 %>% 
+  group_by(chr,pos) %>% 
+  nest() %>% 
+  mutate(allele = map(data,haplogroup_name)) %>% 
+  unnest() 
+
+###now isolate all the haplotype frequency data in the window of interest
+cent.200 <- rbind(filter(hap200, chr == "chr3R", pos < 7307440, !is.na(freq)),
+                  filter(hap200, chr == "chr3L", pos > 22586280, !is.na(freq)))
+
+### sum the haplotype frequencies of haplotypes that cannot be distinguished to generate the frequency of the haplogroup.
+cent.200 <- left_join(cent.200,select(cent.200.haplogroups,-freq)) %>% group_by(chr,pos,pool,haplogroup) %>%
+  summarise(freq_sum = sum(freq, na.rm = TRUE))
+
+
+#repeat for haplotypes based on 500 Kb windows
+cent.500 <- rbind(filter(hap500, chr == "chr3R", pos < 7307440, !is.na(freq), pool== "A.C") %>% select(-pool),
+                  filter(hap500, chr == "chr3L", pos > 22586280, !is.na(freq), pool== "A.C") %>% select(-pool))
+
+cent.500.haplogroups = cent.500 %>% 
+  group_by(chr,pos) %>% 
+  nest() %>% 
+  mutate(allele = map(data,haplogroup_name)) %>% 
+  unnest() 
+
+cent.500 <- rbind(filter(hap500, chr == "chr3R", pos < 7307440, !is.na(freq)),
+                  filter(hap500, chr == "chr3L", pos > 22586280, !is.na(freq)))
+
+cent.500 <- left_join(cent.500,select(cent.500.haplogroups,-freq)) %>% group_by(chr,pos,pool,haplogroup) %>%
+  summarise(freq_sum = sum(freq, na.rm = TRUE))
+
+
+#repeat for haplotypes based on 1M windows
+cent.1000 <- rbind(filter(hap1000, chr == "chr3R", pos < 7307440, !is.na(freq), pool== "A.C") %>% select(-pool),
+                   filter(hap1000, chr == "chr3L", pos > 22586280, !is.na(freq), pool== "A.C") %>% select(-pool))
+
+cent.1000.haplogroups = cent.1000 %>% 
+  group_by(chr,pos) %>% 
+  nest() %>% 
+  mutate(allele = map(data,haplogroup_name)) %>% 
+  unnest() 
+
+cent.1000 <- rbind(filter(hap1000, chr == "chr3R", pos < 7307440, !is.na(freq)),
+                  filter(hap1000, chr == "chr3L", pos > 22586280, !is.na(freq)))
+
+cent.1000 <- left_join(cent.1000,select(cent.1000.haplogroups,-freq)) %>% group_by(chr,pos,pool,haplogroup) %>%
+  summarise(freq_sum = sum(freq, na.rm = TRUE))
+
+#now we are ready to reanalyze the data, with haplogroup as a predictor instead of haplotype the anova function, significance is determined from the interaction term between haplogroup and treatment
+anova_full <- function(df){
+  df = data.frame(df)
+  df$Nrep = as.factor(df$rep)
+  df$trt = as.factor(df$trt)
+  df$haplogroup = as.factor(df$haplogroup)
+  df<- df %>% 
+    mutate(afreq=asin(sqrt(freq_sum))) %>%
+    select(-c(Nrep,freq_sum))
+  tt <- tapply(df$afreq,df$haplogroup,mean) #get the mean of afreq for each haplotype
+  tt2 <- names(tt)[tt > 0.14] #keep haplogroups whose afreq greater than 0.14 (haplotypes > 0.02)
+  df2 <-  df %>% filter(haplogroup %in% tt2) %>% droplevels() #remove haplotypes which are not in tt2
+  out = anova(lm(afreq~haplogroup+haplogroup:trt+haplogroup:rep,data=df2))
+  -pf(out[2,3]/out[4,3],out[2,1],out[4,1],lower.tail=FALSE,log.p=TRUE)/log(10)	
+}
+
+haplogroup.LODS.200 <- cent.200  %>%  
+  separate(pool,c("rep", "trt")) %>% 
+  group_by(chr,pos) %>% 
+  nest() %>% 
+  mutate(LOD_full = map(data,anova_full)) %>% 
+  select(-data) %>%
+  unnest(cols = c(LOD_full)) %>%
+  mutate(Ichr=recode(chr,'chrX'=1,'chr2L'=2,'chr2R'=3,'chr3L'=4,'chr3R'=5)) %>%
+  unite("ID", chr:pos, remove = FALSE)
+
+haplogroup.LODS.200$window.size <- "200 Kb"
+
+
+haplogroup.LODS.500 <- cent.500  %>%  
+  separate(pool,c("rep", "trt")) %>% 
+  group_by(chr,pos) %>% 
+  nest() %>% 
+  mutate(LOD_full = map(data,anova_full)) %>% 
+  select(-data) %>%
+  unnest(cols = c(LOD_full)) %>%
+  mutate(Ichr=recode(chr,'chrX'=1,'chr2L'=2,'chr2R'=3,'chr3L'=4,'chr3R'=5)) %>%
+  unite("ID", chr:pos, remove = FALSE)
+
+haplogroup.LODS.500$window.size <- "500 Kb"
+
+
+haplogroup.LODS.1000 <- cent.1000  %>%  
+  separate(pool,c("rep", "trt")) %>% 
+  group_by(chr,pos) %>% 
+  nest() %>% 
+  mutate(LOD_full = map(data,anova_full)) %>% 
+  select(-data) %>%
+  unnest(cols = c(LOD_full)) %>%
+  mutate(Ichr=recode(chr,'chrX'=1,'chr2L'=2,'chr2R'=3,'chr3L'=4,'chr3R'=5)) %>%
+  unite("ID", chr:pos, remove = FALSE)
+
+haplogroup.LODS.1000$window.size <- "1000 Kb"
+
+# NEXT we loess smooth lod scores for all 3 haplotype windows
+
+#drop chromosomes not represented from the data
+haplogroup.LODS.200$chr <- droplevels(haplogroup.LODS.200$chr)
+
+#for 200 Kb windows
+# SET "span" value for loess smoothing
+# - using a chromosome arm-specific value
+#   so the # of markers for the fit is always
+#   approx the same (at ~21)
+# - assumes number of rows per chromosome arm is given by the counts in this table:
+table(haplogroup.LODS.200$chr)
+chr_ids <- c("chr3L","chr3R")
+chr_loess_spans <- matrix(c(0.09606987, 0.09361702),ncol=1)
+rownames(chr_loess_spans) <- chr_ids
+colnames(chr_loess_spans) <- "loess_span_val"
+chr_loess_spans
+
+###now we loess smooth the LOD scores
+outLODS.200 <- NULL
+for(chr in levels(haplogroup.LODS.200$chr)){
+  temp = haplogroup.LODS.200[haplogroup.LODS.200$chr==chr,] # EXTRACT 1 chromosome arm of data
+  
+  # Next line loess smoothing 
+  # - degree = 2 (degree of polynomial; can be 1 or 2)
+  # - span = a proportion (If # of points is N and span=0.5, then for given
+  #   position (x) loess will use the 0.5 * N closest datapoints to x for the fit)
+  fit1 <- loess(temp$LOD_full ~ temp$pos, degree=2, span = chr_loess_spans[rownames(chr_loess_spans)==chr,], family="gaussian")
+  temp$smooLOD = fit1$fitted
+  # KEEP the newly-smoothed dataset
+  outLODS.200 <- rbind(outLODS.200,temp)
+}
+
+outLODS.200$window.size <- "200 Kb"
+
+
+#for 500 Kb windows
+haplogroup.LODS.500$chr <- droplevels(haplogroup.LODS.500$chr)
+table(haplogroup.LODS.500$chr)
+chr_ids <- c("chr3L","chr3R")
+chr_loess_spans <- matrix(c(0.0876494, 0.09243697),ncol=1)
+rownames(chr_loess_spans) <- chr_ids
+colnames(chr_loess_spans) <- "loess_span_val"
+chr_loess_spans
+outLODS.500 <- NULL
+for(chr in levels(haplogroup.LODS.500$chr)){
+  temp = haplogroup.LODS.500[haplogroup.LODS.500$chr==chr,] 
+  fit1 <- loess(temp$LOD_full ~ temp$pos, degree=2, span = chr_loess_spans[rownames(chr_loess_spans)==chr,], family="gaussian")
+  temp$smooLOD = fit1$fitted
+  outLODS.500 <- rbind(outLODS.500,temp)
+}
+outLODS.500$window.size <- "500 Kb"
+
+#for 1000 Kb windows
+haplogroup.LODS.1000$chr <- droplevels(haplogroup.LODS.1000$chr)
+table(haplogroup.LODS.1000$chr)
+chr_ids <- c("chr3L","chr3R")
+chr_loess_spans <- matrix(c(0.088, 0.088),ncol=1)
+rownames(chr_loess_spans) <- chr_ids
+colnames(chr_loess_spans) <- "loess_span_val"
+chr_loess_spans
+outLODS.1000 <- NULL
+for(chr in levels(haplogroup.LODS.1000$chr)){
+  temp = haplogroup.LODS.1000[haplogroup.LODS.1000$chr==chr,] 
+  fit1 <- loess(temp$LOD_full ~ temp$pos, degree=2, span = chr_loess_spans[rownames(chr_loess_spans)==chr,], family="gaussian")
+  temp$smooLOD = fit1$fitted
+  outLODS.1000 <- rbind(outLODS.1000,temp)
+}
+outLODS.1000$window.size <- "1 Mb"
+
+
+#join the data into a single object for graphing
+cent.3 <- rbind(outLODS.200,outLODS.500,outLODS.1000)
+#order window size factor levels so they are logical
+cent.3$window.size <- factor(cent.3$window.size, levels = c("200 Kb", "500 Kb", "1 Mb"))
+
+### the delta 3LOD window can be determined by manual inspection of the the outLODS objects. Here we draw a box that will represent the interval graphically. For 500 Kb windows is is 3L:25576280
+rect.tbl <- tibble(x1 = c(25586280,2307440), x2=c(27866280,3467440), y1 = rep(0,2), y2 = rep(5,2),chr=c("chr3L","chr3R"))
+
+#build plot
+ggplot(cent.3, aes(pos,smooLOD,color=window.size)) +
+  geom_rect(data=rect.tbl,aes(xmin=x1,xmax=x2,ymin=y1,ymax=y2),fill="#866e3c", alpha=.2,inherit.aes = FALSE) +geom_line() + 
+   theme_classic() + facet_grid(~chr,scales="free") +
+   scale_color_manual(values=c("#Db2f18","#db751b","#86443c")) + 
+  geom_hline(yintercept = 4, linetype = "dashed", colour = "#866e3c") +  
+   theme_classic() + ylab("-log10(p)\n") + theme(legend.position="bottom")+
+   xlab("\nPhysical Location (Mb)") + scale_x_continuous(breaks = c(25586280,3467440))
+
+ggsave("haplogroup_mapping.pdf",width=3.5,height=3)
+
+### in the second part of this script we evaluate how haplotype frequencies differ between experimental and control samples 
+
+
+Fs.500 = cent.500 %>% filter(!is.na(freq_sum)) %>% 
+  separate(pool,c("rep","trt")) %>%
+  filter(chr != "chr4") %>%
+  droplevels() %>%
+  pivot_wider(names_from = trt, values_from = freq_sum) %>%
+  mutate(freqdff = T/C) %>%
+  select(chr,pos,haplogroup,freqdff,rep) 
+
+##lod peak in 500 Kb mapping is chr3R 3317440
+peak3R.500.Fs <- filter(Fs.500, chr == "chr3R", pos == 3317440)
+#change B2 to B6 because these two haplotygroups cannot be separated and this more logical colors in the combined plot
+peak3R.500.Fs$haplogroup[peak3R.500.Fs$haplogroup=="B2"] <- "B6"
+
+###inspect the raw haplotype frequencies at this position to determine who should be dropped and which haplotypes are grouped
+filter(hap500, chr == "chr3R", pos == 3317440)
+#B1/B5 are basically absent freq << 0.02
+#B3/B4 are a haplogroup, we can't tell them apart
+#B2/B6 are a haplogroup, we can't tell them apart.
+#change B2 to B6 because these two haplotygroups cannot be separated and this more logical colors in a combined plot
+peak3R.500.Fs$haplogroup[peak3R.500.Fs$haplogroup=="B2"] <- "B6"
+
+
+##lod peak in 500 Kb mapping is chr3L 3317440
+peak3L.500.Fs <- filter(Fs.500, chr == "chr3L", pos == 25716280)
+
+###inspect the raw haplotype frequencies at this position to determine who should be dropped and which haplotypes are grouped
+filter(hap500, chr == "chr3L", pos == 25716280)
+#B1/B5 are basically absent freq < 0.02
+#all other haplotypes can be resolved
+
+#make a combined table for a faceted plot 
+peak.500.Fs <- rbind(peak3R.500.Fs,peak3L.500.Fs)
+
+ggplot(filter(peak.500.Fs,haplogroup != "B1", haplogroup != "B5"), aes(haplogroup, freqdff, colour = haplogroup)) +
+  theme_classic() +
+  stat_summary(geom="bar",fun="mean",fill = "white",size=1.5) +
+  geom_point(size=1.5) +
+  scale_color_manual(values=c("#db715b","#86443c","#332f2c","#338a7d","#Db2f18","#866e3c")) +theme(legend.position = "none") +
+  geom_hline(yintercept=1,linetype=2) +
+  facet_grid(~chr,scales="free",space="free") + 
+  xlab("\nfounder haplotype") + ylab("relative haplotype frequency (T/C)\n") 
+ggsave("deltafreq.pdf", height =3, width=3.5)
+
+
+
+
+
+#reanalyze the data to see if some haplotypes are more important than others 
+#drop B1/B5 because not present
+peak.500 <- filter(cent.500, chr == "chr3R", pos == 3317440,haplogroup != "B1",haplogroup != "B5")
+peak.500$afreq <- asin(sqrt(peak.500$freq_sum))
+peak.500 <- peak.500 %>% separate(pool,c("rep","trt"))
+peak.500.lm <- lm(afreq~haplogroup+haplogroup:trt+haplogroup:rep,peak.500)
+-pf(anova(peak.500.lm)[2,3]/anova(peak.500.lm)[4,3],anova(peak.500.lm)[2,1],anova(peak.500.lm)[4,1],lower.tail=FALSE,log.p=TRUE)/log(10)
+
+
+###drop B2/B6 LOD score drops 
+peak.500.lm <- lm(afreq~haplogroup+haplogroup:trt+haplogroup:rep,filter(peak.500,haplogroup != "B2"))
+anova(peak.500.lm)
+-pf(anova(peak.500.lm)[2,3]/anova(peak.500.lm)[4,3],anova(peak.500.lm)[2,1],anova(peak.500.lm)[4,1],lower.tail=FALSE,log.p=TRUE)/log(10)
+
+
+###drop B3/B4 LOD score increases
+peak.500.lm <- lm(afreq~haplogroup+haplogroup:trt+haplogroup:rep,filter(peak.500,haplogroup != "B3"))
+anova(peak.500.lm)
+-pf(anova(peak.500.lm)[2,3]/anova(peak.500.lm)[4,3],anova(peak.500.lm)[2,1],anova(peak.500.lm)[4,1],lower.tail=FALSE,log.p=TRUE)/log(10)
+
+###drop B7 LOD score drops 
+peak.500.lm <- lm(afreq~haplogroup+haplogroup:trt+haplogroup:rep,filter(peak.500,haplogroup != "B7"))
+anova(peak.500.lm)
+-pf(anova(peak.500.lm)[2,3]/anova(peak.500.lm)[4,3],anova(peak.500.lm)[2,1],anova(peak.500.lm)[4,1],lower.tail=FALSE,log.p=TRUE)/log(10)
+
+###drop B8 LOD score drops, still significant
+peak.500.lm <- lm(afreq~haplogroup+haplogroup:trt+haplogroup:rep,filter(peak.500,haplogroup != "B8"))
+anova(peak.500.lm)
+-pf(anova(peak.500.lm)[2,3]/anova(peak.500.lm)[4,3],anova(peak.500.lm)[2,1],anova(peak.500.lm)[4,1],lower.tail=FALSE,log.p=TRUE)/log(10)
+
+
+
+peak.3R.500 <- filter(cent.500, chr == "chr3R", pos == 3317440,haplogroup != "B1",haplogroup != "B5") %>% separate(pool,c("rep","trt")) %>% filter(trt=="C")
+peak.3R.500$haplogroup[peak.3R.500$haplogroup=="B2"] <- "B6"
+peak.3L.500 <- filter(cent.500, chr == "chr3L", pos == 25716280,haplogroup != "B1",haplogroup != "B5") %>% separate(pool,c("rep","trt")) %>% filter(trt=="C")
+peak.500 <- rbind(peak.3R.500,peak.3L.500)
+ggplot(peak.500, aes(x=rep,y=freq_sum,fill=haplogroup)) + 
+  geom_bar(stat="identity") + 
+  scale_fill_manual(values=c("#db715b","#86443c","#332f2c","#338a7d","#Db2f18","#866e3c")) +
+  theme_classic() + xlab("\nreplicate") +
+  facet_grid(~chr)+
+  ylab("haplogroup frequency\n")
+
+ggsave("haplogroup_freq.pdf", height = 3, width = 4 )
+
+
---
+++ XQTL-mapping/R-scripts/candidate_variants.R
@@ -0,0 +1,125 @@
+ssetwd("~/Documents/manuscripts/XQTL/G3_submission/github/final/XQTL-mapping/data-files")
+library(tidyverse)
+
+#full list of 2701 alternate alleles
+genos <- read_delim("split.filtered.B6neB7.vcf",delim="\t",col_names=TRUE,skip=22)
+names(genos)[4] <- "Ref"
+
+## determine the length of the alternate allele, this is helpful for categorizing the type of variant
+genos$reflength <- nchar(genos$Ref)
+genos$altlength <- nchar(genos$ALT)
+
+#determine the class of variant based on length
+genos$variant_type <- "unclear"
+genos$variant_type[genos$reflength == 1 & genos$altlength== 1] <- "SNP"
+genos$variant_type[genos$variant_type != "SNP" & genos$reflength < 10 & abs(genos$reflength-genos$altlength)<= 100] <- "small INDEL"
+genos$variant_type[genos$variant_type != "SNP" & genos$altlength < 10 & abs(genos$reflength-genos$altlength) <= 100] <- "small INDEL"
+genos$variant_type[genos$reflength < 10 & abs(genos$reflength-genos$altlength)> 100] <- "large INDEL"
+genos$variant_type[genos$altlength < 10 & abs(genos$reflength-genos$altlength)> 100] <- "large INDEL"
+genos$variant_type[genos$reflength > 10*genos$altlength & genos$reflength> 100] <- "large INDEL"
+genos$variant_type[genos$altlength > 10*genos$reflength & genos$altlength> 100] <- "large INDEL"
+genos$variant_type[genos$reflength >100 & genos$altlength> 100] <- "complex"
+
+write_csv(genos,"genos.annotated.vcf")
+
+#manually inspect the "complex" and "unclear" variants, often these reflect positions where there is an indel and then nested variation in the longer allele. Also delete the alleles where neither B2 nor B6 is ALT. THe curated alleles are then run through VEP to relate them to gene function.
+#import my cleaned clean version of the data
+genos <- read_csv("genos.annotated.curated.csv")
+##there are 2132 lines, i.e. variants in the file, which correspond to 2046 variable positions in dm6
+pull(genos,ID) %>% unique() %>% length()
+genos$variant_type <- factor(genos$variant_type, levels = c("SNP","small INDEL","large INDEL","small ambiguous","large ambiguous"))
+
+
+# read in VEP output with variant effects
+VEP <- read_csv("VEP.csv",col_names=TRUE) 
+names(VEP)[1] <- "ID"
+
+#merge VEP with molecular descriptions and genotypes. Note that the merged table is longer than any individual table because variant positions can have multiple effects (in VEP) and also multiple alleles (in genos)
+merged <-left_join(VEP,genos)
+
+# quantify the number of variants with each type of effect
+select(merged,ID, Consequence) %>% distinct() %>% pull(Consequence) %>% table()
+
+#make a new column "functional" describing whether a variant may impact ANY genes (i.e. is it within a gene body or 5Kb of any encoded transcript). If it is not is labelled as intergenic by VEP
+merged$functional <- "genic"
+merged$functional[merged$Consequence == "intergenic_variant"] <- "intergenic"
+
+#now make a new table where very variable position is described a single time as either "genic" or "intergenic", note this table has 2050 variable positions because a handfull of variants have alleles of different types
+
+variant.tbl <- select(merged,ID,variant_type,functional) %>% distinct()
+#how many of each class of variant
+pull(variant.tbl,variant_type) %>% table() %>% sort(decreasing=TRUE)
+#percentage of variants that are SNps and small INDELs
+(1211+772)/2050
+
+pull(variant.tbl,functional) %>% table() %>% sort(decreasing=TRUE)
+#percentage of variants that are SNps and small INDELs
+1215/2050
+
+
+#make the graph
+ggplot(variant.tbl,aes(variant_type,fill=functional)) + geom_bar() +
+  scale_fill_manual(values=c("#18dbbe","#2d3d3b")) + theme_classic() +
+  xlab("") + theme(axis.text.x=element_text(angle=90,hjust=1)) +
+  ylab("candidate variant count\n")
+ggsave("variant_effects.pdf",height=3,width=3)
+
+#make a table describing whether different types of effects are observed in each gene 
+gene.impacts <- filter(merged, Consequence != "intergenic_variant") %>% select(SYMBOL,Consequence)  %>% distinct() %>% mutate(dummy="yes") %>% pivot_wider(names_from = Consequence,values_from = dummy, values_fill = "no") 
+
+#intron_variant column should read yes for the non-coding RNAs. We can drop the column intron_variant&non_coding_transcript_variant after we make this change
+gene.impacts$intron_variant[gene.impacts$`intron_variant&non_coding_transcript_variant` =="yes"] <- "yes"
+gene.impacts<- select(gene.impacts, -`intron_variant&non_coding_transcript_variant`)
+
+#fix gene name for sph242
+gene.impacts$SYMBOL[gene.impacts$SYMBOL == "CG40160"] <- "Sph242"
+
+#add row for CR41623
+names(gene.impacts)
+
+gene.impacts <- rbind(gene.impacts, tibble(SYMBOL = "CR46123",intron_variant = "no", upstream_gene_variant = "no", 
+                 downstream_gene_variant = "no", synonymous_variant = "no", non_coding_transcript_exon_variant = "no",
+                 `5_prime_UTR_variant` = "no", `3_prime_UTR_variant` = "no", missense_variant = "no"))
+
+#add information about differential expression
+gene.impacts$DEG <- "not DEG"
+gene.impacts$DEG[gene.impacts$SYMBOL == "Pzl"] <- "up in B7"
+gene.impacts$DEG[gene.impacts$SYMBOL == "l(3)80Fg"] <- "up in B7"
+gene.impacts$DEG[gene.impacts$SYMBOL == "spok"] <- "up in B7"
+gene.impacts$DEG[gene.impacts$SYMBOL == "lovit"] <- "up in B7"
+gene.impacts$DEG[gene.impacts$SYMBOL == "CR42650"] <- "up in B7"
+gene.impacts$DEG[gene.impacts$SYMBOL == "Pzl"] <- "up in B7"
+gene.impacts$DEG[gene.impacts$SYMBOL == "Myo81F"] <- "up in B6"
+gene.impacts$DEG[gene.impacts$SYMBOL == "Dbp80"] <- "up in B6"
+gene.impacts$DEG[gene.impacts$SYMBOL == "Sph242"] <- "up in B6"
+gene.impacts$DEG[gene.impacts$SYMBOL == "CR46123"] <- "up in B6"
+
+#now we have to pivot long again to make the graph
+gene.impacts <- pivot_longer(gene.impacts, !SYMBOL, names_to ="impact",values_to = "fill")
+
+#cleanup and reorder names
+gene.impacts$impact <- gsub("3_prime","3'",gene.impacts$impact)
+gene.impacts$impact <- gsub("5_prime","5'",gene.impacts$impact)
+gene.impacts$impact <- gsub("_variant","",gene.impacts$impact)
+gene.impacts$impact <- gsub("_gene","",gene.impacts$impact)
+gene.impacts$impact <- gsub("_UTR","-UTR",gene.impacts$impact)
+gene.impacts$impact <- gsub("non_coding_transcript_exon","ncRNA-transcript",gene.impacts$impact)
+levels(as.factor(gene.impacts$impact))
+gene.impacts$impact <- factor(gene.impacts$impact, levels=c("missense","synonymous","5'-UTR","3'-UTR","ncRNA-transcript","intron","upstream","downstream","DEG"))
+gene.impacts$fill <- factor(gene.impacts$fill, levels=c("no","yes","not DEG","up in B6","up in B7"))
+
+#make a variable to describe facets
+gene.impacts$facet <- "variants"
+gene.impacts$facet[gene.impacts$impact=="DEG"] <- "DEG"
+gene.impacts$facet <- factor(gene.impacts$facet, levels=c("variants","DEG"))
+
+#now we can make a tile graph relating the presence of different types of variants to genes within the QTL
+ggplot(gene.impacts, aes(x=impact,y=SYMBOL,fill=fill))+  
+  geom_tile() + theme_classic() +
+  facet_wrap(~facet,scales="free_x",space="free_x")+
+  theme(axis.text.x=element_text(angle=90,hjust=1),axis.text.y=element_text(face="italic")) +
+  scale_fill_manual(values=c("#332f2c","#18dbbe","#2d3d3b","#338a7d","#Db2f18")) +
+  xlab("") + ylab("")
+ggsave("impacted_genes.pdf",height=4.5,width=4)
+
+
